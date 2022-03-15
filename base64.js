e9a4b2b61ee67cc59d7bc7c37c48c5d54025214f
付明 <mingwiki@gmail.com>
2022-03-16 00:32:59 +0800

    Update base64.js

--- base64.js
+++ base64.js
@@ -1,9 +1,15 @@
 const fs = require('fs');
 
 let rules = fs.readFileSync('rules', 'utf8')
-let str = "[AutoProxy]\n" + rules
+let sortedRules = rules.split("\n").filter(e => e !== '' ).sort()
+let str = "[AutoProxy]\n" + sortedRules
+
+fs.writeFile('rules', sortedRules, function (err) {
+  if (err) throw err;
+  console.log('Rules updated!');
+});
 
 fs.writeFile('list.txt', btoa(str), function (err) {
   if (err) throw err;
-  console.log('File updated!');
+  console.log('List updated!');
 });
