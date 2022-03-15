4837d0c84a12d42cfb10bfd2a0e8a51fb5d21ae6
mingwiki <mingwiki@gmail.com>
2022-03-15 23:56:35 +0800

    Revert "test"
    This reverts commit 1f86963b098969c410855faeb5e16106b02f9839.

--- base64.js
+++ base64.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 let rules = fs.readFileSync('rules', 'utf8')
-let str = "[Switchy]\n" + rules
+let str = "[AutoProxy]\n" + rules
 
 fs.writeFile('list.txt', btoa(str), function (err) {
   if (err) throw err;
