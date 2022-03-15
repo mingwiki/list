1f86963b098969c410855faeb5e16106b02f9839
mingwiki <mingwiki@gmail.com>
2022-03-15 23:39:54 +0800

    test

--- base64.js
+++ base64.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 let rules = fs.readFileSync('rules', 'utf8')
-let str = "[AutoProxy]\n" + rules
+let str = "[Switchy]\n" + rules
 
 fs.writeFile('list.txt', btoa(str), function (err) {
   if (err) throw err;
