ab5971c73d5427d3803612ddb941128bd9013111
付明 <mingwiki@gmail.com>
2022-03-15 21:11:10 +0800

    Create base64.js

---
+++ base64.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+
+let rules = fs.readFileSync('rules', 'utf8')
+let str = "[autoproxy]\n" + rules
+
+fs.writeFile('list.txt', btoa(str), function (err) {
+  if (err) throw err;
+  console.log('File updated!');
+});
