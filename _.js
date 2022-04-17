c37e6cc88e696e40ce1f71a5b61e068d3977c3c4
mingwiki <mingwiki@gmail.com>
2022-04-17 22:17:20 +0800

    fix delete logic

--- _.js
+++ _.js
@@ -3,7 +3,7 @@ const fs = require("fs")
 let _add = fs.readFileSync("_add.txt", "utf8")
 let _delete = fs.readFileSync("_delete.txt", "utf8")
 let _list = Buffer.from(fs.readFileSync("list.txt", 'utf8'), "base64").toString('utf8')
-let list = new Array("[AutoProxy]").concat(_list.split("\n").slice(1).concat(Array.from(new Set(_add.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n').filter(e => !_delete.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n').includes(e)))).filter(e => e !== "")).sort().filter(e => e.length)).join("\n")
+let list = new Array("[AutoProxy]").concat(_list.split("\n").slice(1).concat(Array.from(new Set(_add.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n'))).filter(e => e !== "")).sort().filter(e => e.length && !(_delete.split("\n").map(e => e.trim().split(' ').filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n').includes(e)))).join("\n")
 
 fs.writeFile("list.txt", Buffer.from(list, 'utf8').toString('base64'), function (err) {
   if (err) throw err
@@ -12,4 +12,8 @@ fs.writeFile("list.txt", Buffer.from(list, 'utf8').toString('base64'), function 
 fs.writeFile("_add.txt", "", function (err) {
   if (err) throw err
   console.log("_add.txt Cleaned!")
+})
+fs.writeFile("_delete.txt", "", function (err) {
+  if (err) throw err
+  console.log("_delete.txt Cleaned!")
 })
