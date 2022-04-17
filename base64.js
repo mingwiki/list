60a9cb9320417b09a6e9634b4d10c7de742b28aa
mingwiki <mingwiki@gmail.com>
2022-04-17 19:57:15 +0800

    update

--- base64.js
+++ base64.js
@@ -3,15 +3,15 @@ const fs = require("fs")
 let addRules = fs.readFileSync("_addRules.txt", "utf8")
 let rules = fs.readFileSync(".rules", "utf8") + '\n' + addRules
 
-let newRules = Array.from(new Set(rules.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join('\n')).join('\n').split('\n'))).filter(e => e !== "").sort().join("\n")
+let newRules = Array.from(new Set(rules.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join('\n')).join('\n').split('\n'))).filter(e => e !== "").sort().map(e => btoa(e)).join("\n")
 let list = "[AutoProxy]\n" + newRules
 
-fs.writeFile(".rules", newRules, function (err) {
+fs.writeFile(".rules", atob(newRules), function (err) {
   if (err) throw err
   console.log(".rules updated!")
 })
 
-fs.writeFile("list.txt", btoa(list), function (err) {
+fs.writeFile("list.txt", list, function (err) {
   if (err) throw err
   console.log("list.txt updated!")
 })
