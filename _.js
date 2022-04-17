a14021c26863b6dd558cd3bf85eac231dfdb02a4
mingwiki <mingwiki@gmail.com>
2022-04-17 21:58:28 +0800

    add delete file

--- _.js
+++ _.js
@@ -1,8 +1,9 @@
 const fs = require("fs")
 
 let _add = fs.readFileSync("_add.txt", "utf8")
+let _delete = fs.readFileSync("_delete.txt", "utf8")
 let _list = Buffer.from(fs.readFileSync("list.txt", 'utf8'), "base64").toString('utf8')
-let list = new Array("[AutoProxy]").concat(_list.split("\n").slice(1).concat(Array.from(new Set(_add.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||'))))).filter(e => e !== "")).sort().filter(e => e.length)).join("\n")
+let list = new Array("[AutoProxy]").concat(_list.split("\n").slice(1).concat(Array.from(new Set(_add.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n').filter(e => !_delete.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n').includes(e)))).filter(e => e !== "")).sort().filter(e => e.length)).join("\n")
 
 fs.writeFile("list.txt", Buffer.from(list, 'utf8').toString('base64'), function (err) {
   if (err) throw err
