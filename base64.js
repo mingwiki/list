5bbeeb8abc7e88bf76bff07bbb51bfe3bcbedc0c
mingwiki <mingwiki@gmail.com>
2022-04-17 20:54:00 +0800

    convert base64 line to line

--- base64.js
+++ base64.js
@@ -1,21 +1,13 @@
 const fs = require("fs")
 
 let _add = fs.readFileSync("_add.txt", "utf8")
-let _rules = fs.readFileSync(".rules", "utf8") + '\n' + _add
-
-let _new = Array.from(new Set(_rules.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join('\n')).join('\n').split('\n'))).filter(e => e !== "").sort().join("\n")
-let list = ("[AutoProxy]\n" + _new).split("\n").map(e => btoa(e)).join("\n")
-
-fs.writeFile(".rules", _rules, function (err) {
-  if (err) throw err
-  console.log(".rules updated!")
-})
+let _list = fs.readFileSync("list.txt", "utf8")
+let list = new Array("[AutoProxy]").concat(_list.split("\n").map(e => atob(e)).slice(1).concat(Array.from(new Set(_add.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||'))))).filter(e => e !== "")).sort().filter(e => e.length)).map(e => btoa(e)).join("\n")
 
 fs.writeFile("list.txt", list, function (err) {
   if (err) throw err
   console.log("list.txt updated!")
 })
-
 fs.writeFile("_add.txt", "", function (err) {
   if (err) throw err
   console.log("_add.txt Cleaned!")
