5c14638d6f8a8b78004f16b2d1121a608b2773d7
mingwiki <mingwiki@gmail.com>
2022-05-04 17:57:45 +0800

    Update _.js

--- _.js
+++ _.js
@@ -1,9 +1,9 @@
 const fs = require("fs")
 
-let _add = fs.readFileSync("_add.txt", "utf8")
-let _delete = fs.readFileSync("_delete.txt", "utf8")
-let _list = Buffer.from(fs.readFileSync("index.html", 'utf8'), "base64").toString('utf8')
-let list = ["[AutoProxy]"].concat([...new Set(_list.split("\n").slice(1).concat([...new Set(_add.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n'))].filter(e => e !== "")).sort().filter(x => x.length && !(_delete.split("\n").map(e => e.trim().split(' ').filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n').includes(x))))]).join("\n")
+const _add = fs.readFileSync("_add.txt", "utf8")
+const _delete = fs.readFileSync("_delete.txt", "utf8")
+const _list = Buffer.from(fs.readFileSync("index.html", 'utf8'), "base64").toString('utf8')
+const list = ["[AutoProxy]"].concat([...new Set(_list.split("\n").slice(1).concat([...new Set(_add.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n'))].filter(e => e !== "")).sort().filter(x => x.length && !(_delete.split("\n").map(e => e.trim().split(' ').filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n').includes(x))))]).join("\n")
 
 fs.writeFile("index.html", Buffer.from(list, 'utf8').toString('base64'), function (err) {
   if (err) throw err
