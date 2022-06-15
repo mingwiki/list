00e5fbfe7c8b1f3b5a4d1de09a8fc6cd94794084
mingwiki <mingwiki@gmail.com>
2022-06-15 10:52:03 +0800

    Update _.js

--- _.js
+++ _.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 
 const _add = fs.readFileSync("_add.txt", "utf8")
 const _delete = fs.readFileSync("_delete.txt", "utf8")
-const _list = Buffer.from(fs.readFileSync("index.html", 'utf8'), "base64").toString('utf8')
+const _list = Buffer.from(fs.readFileSync("index", 'utf8'), "base64").toString('utf8')
 const list = ["[AutoProxy]"].concat([...new Set(_list.split("\n").slice(1).concat([...new Set(_add.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n'))].filter(e => e !== "")).sort().filter(x => x.length && !(_delete.split("\n").map(e => e.trim().split(' ').filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n').includes(x))))]).join("\n")
 
 fs.writeFile("index", Buffer.from(list, 'utf8').toString('base64'), function (err) {
