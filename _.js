791448ceb2fd81a2964490a0834791b22e231926
mingwiki <mingwiki@gmail.com>
2022-08-24 13:37:20 +0800

    format

--- _.js
+++ _.js
@@ -1,19 +1,69 @@
-const fs = require("fs")
+const fs = require('fs')
+const { readFileSync, writeFile } = fs
+const _add = readFileSync('_add.txt', 'utf8')
+const _delete = readFileSync('_delete.txt', 'utf8')
+const _list = Buffer.from(
+  readFileSync('index.html', 'utf8'),
+  'base64',
+).toString('utf8')
+const list = ['[AutoProxy]']
+  .concat([
+    ...new Set(
+      _list
+        .split('\n')
+        .slice(1)
+        .concat(
+          [
+            ...new Set(
+              _add
+                .split('\n')
+                .map((e) =>
+                  e
+                    .trim()
+                    .split(' ')
+                    .map((e) => e.replace(/^(\*\.)/, '||'))
+                    .filter((e) => e.startsWith('||'))
+                    .join('\n'),
+                )
+                .join('\n')
+                .split('\n'),
+            ),
+          ].filter((e) => e !== ''),
+        )
+        .sort()
+        .filter(
+          (x) =>
+            x.length &&
+            !_delete
+              .split('\n')
+              .map((e) =>
+                e
+                  .trim()
+                  .split(' ')
+                  .filter((e) => e.startsWith('||'))
+                  .join('\n'),
+              )
+              .join('\n')
+              .split('\n')
+              .includes(x),
+        ),
+    ),
+  ])
+  .join('\n')
 
-const _add = fs.readFileSync("_add.txt", "utf8")
-const _delete = fs.readFileSync("_delete.txt", "utf8")
-const _list = Buffer.from(fs.readFileSync("index.html", 'utf8'), "base64").toString('utf8')
-const list = ["[AutoProxy]"].concat([...new Set(_list.split("\n").slice(1).concat([...new Set(_add.split("\n").map(e => e.trim().split(' ').map(e => e.replace(/^(\*\.)/, '||')).filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n'))].filter(e => e !== "")).sort().filter(x => x.length && !(_delete.split("\n").map(e => e.trim().split(' ').filter(e => e.startsWith('||')).join("\n")).join("\n").split('\n').includes(x))))]).join("\n")
-
-fs.writeFile("index.html", Buffer.from(list, 'utf8').toString('base64'), function (err) {
+writeFile(
+  'index.html',
+  Buffer.from(list, 'utf8').toString('base64'),
+  function (err) {
+    if (err) throw err
+    console.log('index.html updated!')
+  },
+)
+writeFile('_add.txt', '', function (err) {
   if (err) throw err
-  console.log("index.html updated!")
+  console.log('_add.txt Cleaned!')
 })
-fs.writeFile("_add.txt", "", function (err) {
+writeFile('_delete.txt', '', function (err) {
   if (err) throw err
-  console.log("_add.txt Cleaned!")
-})
-fs.writeFile("_delete.txt", "", function (err) {
-  if (err) throw err
-  console.log("_delete.txt Cleaned!")
+  console.log('_delete.txt Cleaned!')
 })
